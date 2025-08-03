c31a91b5068be56b7961fe0341f48029c5e5008c
Prince200510 <princemaurya8879@gmail.com>
2025-08-03 11:24:59 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-8xl text-orange-600 font-bold">Welcome Prince Maurya</h1>
+    </div>
+  )
+}
+
+export default App
